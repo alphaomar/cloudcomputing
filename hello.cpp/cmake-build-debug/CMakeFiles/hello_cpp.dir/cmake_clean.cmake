23ddccb8d26faf517454a2ff51ed201921566bd4
leighomar12654@gmail.com <leighomar12654@gmail.com>
2022-01-12 09:15:59 -0800

    My first commit

---
+++ hello.cpp/cmake-build-debug/CMakeFiles/hello_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cpp.dir/main.cpp.obj"
+  "hello_cpp.exe"
+  "hello_cpp.exe.manifest"
+  "hello_cpp.lib"
+  "hello_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
